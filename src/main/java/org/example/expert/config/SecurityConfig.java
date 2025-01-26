f2ffaaabd4abad2dece42f80dd8bdc1ca4223b52
joacho <joacho1205@gmail.com>
2025-01-26 20:56:05 +0900

    lv2-9. Spring Security

---
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package org.example.expert.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+}
